a6d3b497dfa13759ba82680dd3ed5b4ad13275cc
gameplan1975 <gameplan1975@gmail.com>
2020-04-16 06:39:06 +0000

    favoの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_024324) do
+ActiveRecord::Schema.define(version: 2020_04_16_051153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 2020_04_16_024324) do
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "blog_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["blog_id"], name: "index_favorites_on_blog_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
