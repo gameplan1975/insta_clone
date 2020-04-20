0be4a1a1f37b89222646683045533fdd06379363
gameplan1975 <gameplan1975@gmail.com>
2020-04-20 02:36:03 +0000

    メールで挫折

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+＊テキストにある日本語化辞書を入れるとVAGRANTが動かなくなるのでコメントアウト
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+＊余裕があれば、いいね！の数を表示できるようにしたい
--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,13 @@ module Insta
       config.load_defaults 5.2
       config.time_zone = 'Tokyo'
       config.active_record.default_timezone = :local
-      config.i18n.default_local = :ja
-      config.load_defaults 5.1
-      config.generators do |g|
-        g.assets false
-        g.helper false
-      end
+      #日本語化辞書入れるとVagrantに不具合あるのでコメントアウト
+      #config.i18n.default_local = :ja
+      #config.load_defaults 5.1
+      #config.generators do |g|
+      #  g.assets false
+      #  g.helper false
+      #end
     end
   end
 end
